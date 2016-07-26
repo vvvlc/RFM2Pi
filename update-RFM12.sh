5830c86d651cf685635f05baf5bb17698138df22
Glyn Hudson <glyn.hudson@gmail.com>
2016-07-26 13:54:23 +0100

    fix update path

--- update-RFM12.sh
+++ update-RFM12.sh
@@ -2,6 +2,6 @@
 
 sudo service emonhub stop
 
-avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:/home/pi/RFM2Pi/firmware/RFM69CW_RF_Demo_ATmega328/RFM12_Demo_ATmega328.cpp.hex
+avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:/firmware/RFM69CW_RF_Demo_ATmega328/RFM12_Demo_ATmega328.cpp.hex
 
 sudo service emonhub start
--- update-RFM69.sh
+++ update-RFM69.sh
@@ -2,6 +2,6 @@
 
 sudo service emonhub stop
 
-avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:/home/pi/RFM2Pi/firmware/RFM69CW_RF_Demo_ATmega328/RFM69CW_RF12_Demo_ATmega328.cpp.hex
+avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:firmware/RFM69CW_RF_Demo_ATmega328/RFM69CW_RF12_Demo_ATmega328.cpp.hex.hex
 
 sudo service emonhub start
